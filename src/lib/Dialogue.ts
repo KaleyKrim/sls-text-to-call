da9cd28c5139a1b10285014b322c00126c27a8e6
Kaley Kriminger <kaley.kriminger@gmail.com>
2020-04-06 13:17:00 -0700

    add excuses

--- src/lib/Dialogue.ts
+++ src/lib/Dialogue.ts
@@ -1,7 +1,7 @@
 export class Dialogue {
 	dialogues = [
-		"testingtesting",
-		"aa aa"
+		"Hello? ...Hello? Yeah, it's me. Listen, I'm in trouble, I need you to come down to the docks right now. Right now! If you don't get down here in 5 minutes, we're gonna lose the whole shipment of papayas. That's right, the whole thing. Get over here as fast as you can.",
+		"Hey girl, I left my wallet at home and I need you to come pay for my pho. I gotta get back to the office, so could you come soon? And by soon I mean now. Like right now. Thanks! See ya girl."
 	]
 
 	constructor() {}
